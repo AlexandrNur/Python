3a48b816100e728cd99255b604de0775f25dbaf8
AlexandrNur <ivanovmayn258@gmail.com>
2024-12-24 16:37:50 +0500

    Поправил второе задание

---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,10 @@
+import math
+
+def square(storona):
+    storona = math.ceil(storona)  # Округляем сторону вверх до ближайшего целого
+    return storona ** 2
+
+user_input = input("Введите сторону квадрата: ").replace(',', '.')
+user_input = float(user_input)
+S = square(user_input)
+print(f"Площадь квадрата со стороной {math.ceil(user_input)} равна: {S}")
